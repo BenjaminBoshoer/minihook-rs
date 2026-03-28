b530dc37cae37ea25f9a2113a85a6f241cea9475
benjaminboshoer <boshoer.beny@gmail.com>
2026-03-28 10:01:53 -0700

    Added DLL functionality - Parsing IMAGE_DOS_HEADER - Extracting e_lfanew - Extracting IMAGE_NT_HEADER

--- minihook_core/src/main.rs
+++ minihook_core/src/main.rs
@@ -1,16 +1,28 @@
 use minihook_core::{minihook::MiniHook, *};
 use std::io::stdin;
 
+use libloading::{Library, Symbol};
+
 
 fn main() {
-    
-    let mut f = MiniHook::new();
+
+    unsafe {
+        let lib = Library::new("C:\\Users\\rwxbeny\\Documents\\Rust\\minihook-rs\\target\\debug\\minihook_payload.dll").unwrap();
+
+        let external_hook: Symbol<unsafe extern "system" fn(&str, &str, &str) -> i32> =
+            lib.get(b"Hook").unwrap();
+
+        let result = external_hook("test", "test", "test");
+        println!("{result}");
+    }
+
+    /*let mut f = MiniHook::new();
     println!("{:?}", f);
 
-    let mut p = match Process::new(12156) {
+    let mut p = match Process::new(5208) {
         Ok(x) => x,
         Err(y) => panic!(),
-    };
+    };*/
 
     let mut buf = String::new();
     stdin().read_line(&mut buf).unwrap();
--- minihook_payload/src/lib.rs
+++ minihook_payload/src/lib.rs
@@ -6,7 +6,8 @@ use windows::{
     core::*,
 };
 use windows::Win32::Foundation::FARPROC;
-use windows::Win32::System::LibraryLoader::{GetModuleFileNameA, GetModuleHandleExA};
+use windows::Win32::System::LibraryLoader::{GetModuleFileNameA, GetModuleHandleA, GetModuleHandleExA};
+use windows::Win32::System::SystemServices::IMAGE_DOS_HEADER;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -17,19 +18,34 @@ struct HookStatus {
 }
 
 #[unsafe(no_mangle)]
-extern "system" fn Hook(module: &str, target_function: &str, payload_function: &str) -> Result<()> {
+extern "system" fn Hook(module: &str, target_function: &str, payload_function: &str) -> i32 {
+    // Get handle to self
+    //let p_handle = unsafe { GetCurrentProcess() };
+
+    let mut v: Vec<HMODULE> = vec!(HMODULE::default(); 1);
+    let result = unsafe{ GetModuleHandleExA(0, None, v.as_mut_ptr()).unwrap() };
+    let module_handle = v[0];
+
+
+    let image_dos_header = (module_handle.0 as *const IMAGE_DOS_HEADER);
+    let lfanew = unsafe { (*image_dos_header).e_lfanew as usize };
+
+    // Add 32-bit handler
+    let image_nt_headers = unsafe { (image_dos_header.add(lfanew)) as *const IMAGE_NT_HEADERS64 };
+
+
+
+
+    // image_nt_header = module_handle.0 + image_dos_header.e_lfanew as *const IMAGE_NT;
     // Get module handle
-    unsafe {
-        IMAGE_DATA_DIRECTORY::default();
-    }
 
     // Get IAT
 
-
+    println!("Success");
     // Find target function
 
     // Swap functions
-    Ok(())
+    return 0;
 }
 
 fn get_module_handle(module: &str) -> Result<HMODULE> {
