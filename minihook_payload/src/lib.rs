cbddc34489f71cb8e032061703cd787cb3a66b18
benjaminboshoer <boshoer.beny@gmail.com>
2026-04-01 08:05:10 -0700

    Edited payload.dll

--- minihook_payload/src/lib.rs
+++ minihook_payload/src/lib.rs
@@ -17,31 +17,47 @@ pub fn add(left: u64, right: u64) -> u64 {
 struct HookStatus {
 
 }
+extern "system" fn Hook(module: &str, target_function: &str, payload_function: &str) -> i32 {
+    let mut base = HMODULE::default();
+    let result = match unsafe { GetModuleHandleExA(0, None, &mut base).unwrap() } {
+        () => { },
+        _ => { return -1 ;}
+    };
+
+    let base = base.0 as *const u8;
+
+
+    return 0;
+}
 
 #[unsafe(no_mangle)]
-extern "system" fn Hook(module: &str, target_function: &str, payload_function: &str) -> i32 {
+extern "system" fn Mess(module: &str, target_function: &str, payload_function: &str) -> i32 {
     // Get handle to self
     //let p_handle = unsafe { GetCurrentProcess() };
 
-    let mut v: Vec<HMODULE> = vec!(HMODULE::default(); 1);
-    let result = unsafe{ GetModuleHandleExA(0, None, v.as_mut_ptr()).unwrap() };
-    let module_handle = v[0];
+    //let mut v: Vec<HMODULE> = vec!(HMODULE::default(); 1);
+    let mut module_handle = HMODULE::default();
+    let result = unsafe{ GetModuleHandleExA(0, None, &mut module_handle).unwrap() };
 
-
-    let image_dos_header = (module_handle.0 as *const IMAGE_DOS_HEADER);
+    let base = module_handle.0 as *const u8;
+    let image_dos_header = (base as *const IMAGE_DOS_HEADER);
     let lfanew = unsafe { (*image_dos_header).e_lfanew as usize };
 
     // 64-bit handler
-    let image_nt_headers = unsafe { (image_dos_header.add(lfanew)) as *const IMAGE_NT_HEADERS64 };
+    let image_nt_headers = unsafe { (base as usize + lfanew) as *const IMAGE_NT_HEADERS64 };
     // TODO: Add 32-bit handler
 
-    let image_optional_header = unsafe { &(*image_nt_headers).OptionalHeader};
-    //let import_dir = unsafe { (image_dos_header.add((*image_optional_header).DataDirectory[2].VirtualAddress)) as *const IMAGE_IMPORT_DESCRIPTOR};
+    let image_optional_header = unsafe { (*image_nt_headers).OptionalHeader };
+    let import_rva = image_optional_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT.0 as usize].VirtualAddress;
 
+    let import_dir = unsafe { (base.add(import_rva as usize)) as *const IMAGE_IMPORT_DESCRIPTOR};
+    let import_dir2 = unsafe { import_dir.add(1)};
+
+    let name = unsafe {(base as usize + (*import_dir).Name as usize) as *const PCSTR} ;
+    //et name_2 = unsafe{name.to_string()};
 
     // image_nt_header = module_handle.0 + image_dos_header.e_lfanew as *const IMAGE_NT;
     // Get module handle
-
     // Get IAT
 
     println!("Success");
